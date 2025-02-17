c0926667ff679134941fa8b596595026e9ff22a1
Camille Piponiot <camille.piponiot@gmail.com>
2025-02-17 13:59:44 +0100

    change productivity equation to standardize STP by thetainf instead of thetainf-theta0

--- R/int_stocks.R
+++ R/int_stocks.R
@@ -1,18 +1,18 @@
 int_stocks <- function(x,
                        tau = 10,
                        lambda = 0.05,
-                       delta = 1,
+                       delta = 0.2,
                        thetainf = 0.5,
                        theta0 = 0.4,
                        omega = 0.02,
                        ba0 = 10) {
   k <- ba0 - thetainf / omega - ((theta0 - thetainf) * lambda) /
     (omega * lambda - 1) -
-    (16 * exp(2) * delta * (thetainf - theta0)) / (tau^2 * (2 * omega - tau)^3)
+    (16 * exp(2) * delta * thetainf) / (tau^2 * (2 * omega - tau)^3)
   return(
     thetainf / omega + k * exp(-omega * x) + ((theta0 - thetainf) * lambda) /
       (omega * lambda - 1) * exp(-lambda * x) + (exp(1 - x / tau))^2 *
-      (2 * delta * (thetainf - theta0)) / (tau^2 * (2 * omega - tau)) *
+      (2 * delta * thetainf) / (tau^2 * (2 * omega - tau)) *
       (x^2 - x * 4 / (2 * omega - tau) + 8 / (2 * omega - tau)^2)
   )
 }
--- R/productivity.R
+++ R/productivity.R
@@ -1,9 +1,12 @@
 productivity <- function(x,
                          tau = 10,
                          lambda = 0.05,
-                         delta = 1,
+                         delta = 0.2,
                          thetainf = 0.5,
                          theta0 = 0.4) {
-  return(theta0 + (thetainf - theta0) *
-           ((1 - exp(-lambda * x)) + delta * ((x / tau) * exp(1 - x / tau))^2))
+  return(
+    theta0 +
+      (thetainf - theta0) * (1 - exp(-lambda * x)) +
+      thetainf * delta * ((x / tau) * exp(1 - x / tau))^2
+  )
 }
