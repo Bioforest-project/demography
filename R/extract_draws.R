5e2f8c98e3c7970503b21521f008c8c904239c56
Camille Piponiot <camille.piponiot@gmail.com>
2025-08-12 18:33:33 +0200

    explore flux parameters and explanatory variables

---
+++ R/extract_draws.R
@@ -0,0 +1,9 @@
+extract_draws <- function(fit, param){
+  df_draw <- fit$draws(variables = param)
+  data.frame(
+    variable = rep(paste0(param, "[", seq_len(dim(df_draw)[3]), "]"), 
+                   each = dim(df_draw)[1]*dim(df_draw)[2]), 
+    value = c(df_draw), 
+    iter = rep(seq_len(dim(df_draw)[1]*dim(df_draw)[2]), dim(df_draw)[3])
+  )
+}
